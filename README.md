859e67c5176662a1a43209d830d8967f68ac2531
SergeyBondarev <bsa0393@gmail.com>
2021-08-02 15:49:41 +0300

    implementation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # types-env
+
+Gives a simple type validation for your environment variables. Zero dependency.
+
+# How to use
+
+```javascript
+    // CommonJS modules
+    const env = require('types-env');
+```
+```javascript
+    // ES6 modules
+    import env from 'types-env';
+```
+```javascript
+    const baseUrl = env('REACT_APP_BASE_URL', 'string', 'http://default-base-url.com');
+    const widthLimit = env('REACT_APP_WIDTH_LIMIT', 'number', 1000);
+    const isActivated = env('REACT_APP_ACTIVATED', 'boolean', false);
+```
+
+Three types are supported: 'string', 'number' and 'boolean'. Default value can be provided as a third argument.
+Throws an error in case of type mismatch. 
---
+++ index.js
@@ -0,0 +1,23 @@
+const env = (key, type, defaultValue) => {
+  if (!process.env[key]) return defaultValue;
+
+  const value = process.env[key];
+  switch (type) {
+    case "string":
+      return value;
+    case "boolean":
+      if (!value) return false;
+      const lower = value.toLowerCase();
+      if (["1", "true", "yes", "y", "ok", "on"].includes(lower)) return true;
+      if (["0", "false", "no", "n", "nok", "off"].includes(lower)) return false;
+      throw new Error(`Invalid env variable ${key}. Expected a boolean: ${value}`);
+    case "number":
+      const parsed = parseInt(value);
+      if (isNaN(parsed)) throw new Error(`Invalid env variable ${key}. Expected a number: ${value}`);
+      return parsed;
+    default:
+      throw new Error(`Unsupported type ${type} for env variable ${key}. Should be 'string', 'boolean' or 'number'`);
+  }
+}
+
+module.exports = env;
